f9d9a5cc26a8b7688a030cc2992038251f36290f
Tsvetan Dyulgerov <tsvetan.dyulgerov@gmail.com>
2025-09-02 13:37:28 +0300

    Added a /me endpoint for user privilages check from the frontend

--- src/main/java/com/heamimont/salesstoreapi/controller/AuthenticationController.java
+++ src/main/java/com/heamimont/salesstoreapi/controller/AuthenticationController.java
@@ -3,11 +3,17 @@ package com.heamimont.salesstoreapi.controller;
 import com.heamimont.salesstoreapi.dto.auth.AuthResponse;
 import com.heamimont.salesstoreapi.dto.auth.LoginRequest;
 import com.heamimont.salesstoreapi.dto.auth.RegisterRequest;
+import com.heamimont.salesstoreapi.dto.user.UserResponseDTO;
+import com.heamimont.salesstoreapi.model.Role;
 import com.heamimont.salesstoreapi.service.AuthenticationService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 
 /**
@@ -46,5 +52,19 @@ public class AuthenticationController {
         return ResponseEntity
                 .status(HttpStatus.CREATED)
                 .body(response);
+    }
+
+    @GetMapping("/me")
+    @CrossOrigin(origins = "*") // allow requests from the frontend
+    public ResponseEntity<UserResponseDTO> getCurrentUser(Authentication authentication) {
+        // Authentication object is automatically populated by Spring Security after JWT verification
+        User user = (User) authentication.getPrincipal();
+        // Map User entity to DTO
+        UserResponseDTO response = new UserResponseDTO(
+                user.getUsername(),
+                Objects.equals(user.getAuthorities().toString(), "[ROLE_ADMIN]") ? Role.ADMIN : Role.USER
+        );
+
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/heamimont/salesstoreapi/dto/user/UserResponseDTO.java
+++ src/main/java/com/heamimont/salesstoreapi/dto/user/UserResponseDTO.java
@@ -1,5 +1,6 @@
 package com.heamimont.salesstoreapi.dto.user;
 import com.heamimont.salesstoreapi.model.Role;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 import java.util.UUID;
@@ -12,4 +13,11 @@ public class UserResponseDTO {
     private String lastName;
     private String email;
     private Role role;
+
+    public UserResponseDTO() {}
+
+    public UserResponseDTO(String username, Role role) {
+        this.username = username;
+        this.role = role;
+    }
 }
